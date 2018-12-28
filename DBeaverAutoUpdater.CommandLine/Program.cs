e1d0d2e8196a7f54af53006131cd723d1ab8f08b
gabriele-ricci-kyklos <gabriele.ricci@kyklos-group.com>
2018-12-28 20:17:16 +0100

    developed Program class

--- DBeaverAutoUpdater.CommandLine/Program.cs
+++ DBeaverAutoUpdater.CommandLine/Program.cs
@@ -1,8 +1,11 @@
-﻿using DBeaverAutoUpdater.Core.BLL;
+﻿using DBeaverAutoUpdater.Core.BE;
+using DBeaverAutoUpdater.Core.BLL;
+using GenericCore.Support;
 using SimpleLogger;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +13,47 @@ namespace DBeaverAutoUpdater.CommandLine
 {
     class Program
     {
+        const string _argCreateConfigFile = "-c";
+
         static void Main(string[] args)
         {
             SimpleLog.SetLogFile(writeText: true, logLevel: SimpleLog.Severity.Exception);
+            IConfigBLL configBLL = new ConfigBLL();
+            IUpdateBLL updateBLL = new UpdateBLL();
 
             try
             {
+                if (args.Length > 0)
+                {
+                    if (args[0] == _argCreateConfigFile)
+                    {
+                        if (args.Length < 3)
+                        {
+                            Console.Error.WriteLine($"Wrong usage of {_argCreateConfigFile}");
+                        }
 
+                        configBLL
+                            .SaveConfiguration
+                            (
+                                new ConfigurationItem
+                                {
+                                    DBeaverInstallPath = args[1].TrimStart().TrimEnd(),
+                                    Architecture = args[2].ToEnum(Architecture.X86)
+                                }
+                            );
+
+                        return;
+                    }
+                }
+
+                if (!configBLL.ConfigFileExists())
+                {
+                    Console.WriteLine($"Configuration file not found, please call with line arguments {_argCreateConfigFile} DBeaverInstallPath Architecture");
+                    return;
+                }
+
+                ConfigurationItem configItem = configBLL.RetrieveConfiguration();
+                updateBLL.UpdateVersion(configItem);
             }
             catch (Exception ex)
             {
